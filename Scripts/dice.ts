ae1368b9bdb851da085571961430956694137bc2
Teun Duynstee <teun@duynstee.com>
2015-02-18 15:33:25 +0100

    working version with first tests

---
+++ Scripts/dice.ts
@@ -0,0 +1,131 @@
+﻿export = Dice;
+module Dice {
+    export class Roll {
+
+        values: number[] = [];
+        constructor(nums) {
+            this.values = nums;
+        }
+        chanceToThrowThis() {
+            var totalDice: number = this.values.reduce((n1, n2) => n1 + n2, 0);
+            var diceToGo = totalDice;
+            var combination = 1;
+            var totalPossibilities = Math.pow(6, totalDice);
+            for (var i = 0; i < this.values.length; i++) {
+                combination *= choose(diceToGo, this.values[i]);
+                diceToGo -= this.values[i];
+            }
+            return (combination / totalPossibilities);
+        }
+        toString() {
+            return this.values.reduce((n1: string, n2: number) => { return n1 + "-" + n2; }, "[") + "]";
+        }
+    }
+
+    export function RollWith (nrOfDice:number){
+        if (nrOfDice == 0) return [];
+        return rollWith(nrOfDice, 6).map(n => new Roll(n));
+    }
+    var rollCache : { [key: string]: number[][]; } = { };
+    var rollWith = (nrOfDice: number, positions: number): Array<number[]> => {
+        var key: string = nrOfDice + "-" + positions;
+        if (!(key in rollCache)) {
+            var result: number[][] = [];
+            if (positions == 1) {
+                result.push([nrOfDice]);
+            } else {
+                for (var i = 0; i <= nrOfDice; i++) {
+                    var innerResult = [];
+                    innerResult.push(i);
+                    var remainder = rollWith(nrOfDice - i, positions - 1);
+                    for (var item in remainder) {
+                        result.push(innerResult.concat(remainder[item]));
+                    }
+                }
+            }
+            rollCache[key] = result;
+        }
+        return rollCache[key];
+    }
+    var factorial = (x: number, lowerBound: number)=>{
+        var fact: number = 1;
+        while (x >= 1 && x > lowerBound) {
+            fact *= x;
+            x--;
+        }
+        return fact;
+    }
+
+    var choose = (n: number, r: number)=>{
+        return (factorial(n, Math.max(n - r, r)) / (factorial(Math.min(n - r, r), 1)));
+    }
+
+    var cache: { [key: string]: DiceSet; } = {};
+
+    export class DiceSet extends Roll {
+        constructor(nums: number[]) {
+            super(nums);
+        }
+        static For(values: number[]): DiceSet {
+            var key: string = values.reduce((r: string, n: number) => r + "-" + n, "");
+            if (!(key in cache)) {
+                cache[key] = new DiceSet(values);
+            }
+            return cache[key];
+        }
+        Cache(): { [key: string]: DiceSet; } { return cache; }
+
+
+        Score(): number {
+            if (this.values[5] == 0) return 0;
+            return this.values.filter((n, i) => i < 5)
+                .reduce((p, n, i) => n * (i + 1) + p, 0)
+                + (this.values[5] * 5);
+        }
+        CurrentValue(): number {
+            var score: number = this.Score();
+            if (score < 21) return 0;
+            if (score < 25) return 1;
+            if (score < 29) return 2;
+            if (score < 33) return 3;
+            return 4;
+        }
+        private expected: number = null;
+        ExpectedValue(): number {
+            var that = this;
+            if (this.expected === null) {
+                var valueForQuit: number = this.CurrentValue();
+                var valueForRoll: number = 0;
+                var rolls: Roll[] = RollWith(this.DiceLeft());
+                for (var i = 0; i < rolls.length; i++) {
+                    var roll = rolls[i];
+                    var picks: DiceSet[] = roll.values.map((v, i) => { return { value: i, nr: v } }).filter(d => d.nr > 0).map(d => {
+                        var resultSet = that.AddDice(d.value, d.nr);
+                        return resultSet;
+                    });
+                    var pick = picks.sort((d1, d2) => d2.ExpectedValue() - d1.ExpectedValue())[0];
+                    valueForRoll += pick.ExpectedValue() * roll.chanceToThrowThis();
+                }
+                this.expected = Math.max(valueForRoll, valueForQuit);
+            }
+            return this.expected;
+        }
+
+        AddDice(value: number, nr: number): DiceSet {
+            var values: number[] = Array.apply(this, this.values);
+            if (values[value] > 0) return DiceSet.Dead();
+            values[value] = nr;
+            return DiceSet.For(values);
+        }
+
+        DiceLeft(): number {
+            return 8 - this.values.reduce((n1, n2) => n1 + n2, 0);
+        }
+        static Dead(): DiceSet { return new DeadSet(); }
+    }
+    class DeadSet extends DiceSet {
+        constructor() { super([]) }
+        ExpectedValue(): number { return 0; }
+    }
+ 
+}
---
+++ tests/wormTests.ts
@@ -0,0 +1,54 @@
+﻿import tsUnit = require("../Scripts/tsUnit/tsUnit")
+import Dice = require("../Scripts/dice")
+export module WormTests {
+    export class ScoreTests extends tsUnit.TestClass {
+
+        allWormsHaveValue40() {
+            var ds = Dice.DiceSet.For([0, 0, 0, 0, 0, 8]);
+            this.areIdentical(40, ds.Score());
+            this.areIdentical(4, ds.CurrentValue());
+        }
+
+        all5NoWormScores0() {
+            var ds = Dice.DiceSet.For([0, 0, 0, 0, 8, 0]);
+            this.areIdentical(0, ds.Score());
+        }
+
+        all5OneWormScores40() {
+            var ds = Dice.DiceSet.For([0, 0, 0, 0, 7, 1]);
+            this.areIdentical(40, ds.Score());
+        }
+    }
+    export class Rolls extends tsUnit.TestClass {
+
+        throw5times5with5() {
+            var roll = new Dice.Roll([0, 0, 0, 0, 5, 0]);
+            this.areIdentical(1/(6*6*6*6*6), roll.chanceToThrowThis() );
+        }
+
+        roll1Gives6() {
+            var combinations = Dice.RollWith(1);
+            this.areIdentical(6, combinations.length);
+        }
+        roll2Gives21() {
+            var combinations = Dice.RollWith(2);
+            this.areIdentical(21, combinations.length);
+        }
+
+    }
+    export class Expectations extends tsUnit.TestClass {
+        neededWormGivesExpectedValue016666() {
+            var ds = new Dice.DiceSet([3, 0, 2, 0, 2, 0]);
+            this.areIdentical(1 / 6, ds.ExpectedValue()); // the only way to score is by throwing a worm: 1/6
+        }
+        completeSetHasExpectationEqualsCurrent() {
+            var ds = new Dice.DiceSet([3, 0, 0, 0, 1, 3]);
+            this.areIdentical(1, ds.ExpectedValue());
+            this.areIdentical(1, ds.CurrentValue());
+        }
+        expectedValueBeforeStart() {
+            var ds = new Dice.DiceSet([0, 0, 0, 0, 0, 0]);
+            this.areIdentical(1.6447296740400994, ds.ExpectedValue());
+        }
+    }
+} 
